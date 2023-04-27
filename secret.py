1550ab17e753c9384746e56ed9ea90b3dce4b334
ChinoTrades <131936870+ChinoTrades@users.noreply.github.com>
2023-04-26 20:28:09 -0700

    python main.py

--- secret.py
+++ secret.py
@@ -1,5 +1,5 @@
 
 class Secret:
 
-    token = '' # discord bot token
+    token = 'MTEwMDk4MzQxMzQ4MzI0NTYwOA.G4CzNY.wBQK81Ydj5Xb8B1kZQHAVQPawJgjOv5alkkZ8k' # discord bot token
     signal_channel_id = None # channel id for server the bot will be talking to
